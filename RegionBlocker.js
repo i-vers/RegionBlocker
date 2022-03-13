38e9c28563517e6b58d6dafbd66c4fbdee94caf6
i-vers <98107484+i-vers@users.noreply.github.com>
2022-03-13 14:27:27 +0200

    Create RegionBlocker.js

---
+++ RegionBlocker.js
@@ -0,0 +1,30 @@
+// Downloads latest jQuery version into the document.
+function downloadjQuery() {
+    document.open();
+    document.write("<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>");
+    document.close();
+}
+
+// Pre-Setting the 'Country' variable.
+function Execute(){
+    var Country = "";
+
+// Requesting for visitor's IP address.
+$.getJSON('https://ipinfo.io/json', function(result) {
+    Country = result.country
+  });
+
+// Checks if the IP Address is located in the blocked region, if true redirects to an error page.
+if (Country == "RU"){ // Change "RU" (Russia) to any Country Code. 
+    console.log("Test")
+}
+
+// If false, prints an legal notice into console.
+// When you track IP's there has to be a warning, so please do not remove/modify the text in any way.
+else{
+    // This is not watermark, its placed here for legal reasons. You can modify the sizes but do not change the text.
+    console.group("%cRegionLocker", "font-size:50px; color: red; font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;");
+    console.log("%cThis site uses RegionLocker. A tool used to block spesific regions from visiting your site.", "font-size: 20px; font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;");
+    console.groupEnd;
+    }
+}
